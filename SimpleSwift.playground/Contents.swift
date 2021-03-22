ebb6b5cb78530484a9ef1e5ee9ac83b58e75b373
Ted Neward <ted@tedneward.com>
2021-03-21 17:17:43 -0700

    Added playground

---
+++ SimpleSwift.playground/Contents.swift
@@ -0,0 +1,47 @@
+print("Welcome to the UW Calculator Playground")
+
+func calculate(_ args: [String]) -> Int {
+    return -1
+}
+
+func calculate(_ arg: String) -> Int {
+    return -1
+}
+
+// -------------------------------------------
+// All of these expressions should return true
+// if you have implemented calculate() correctly
+//
+calculate(["2", "+", "2"]) == 4
+calculate(["2", "-", "2"]) == 0
+calculate(["2", "*", "2"]) == 4
+calculate(["2", "/", "2"]) == 1
+calculate(["2", "%", "2"]) == 0
+
+calculate(["1", "2", "3", "count"]) == 3
+calculate(["1", "2", "3", "4", "5", "count"]) == 5
+calculate(["count"]) == 0
+
+calculate(["1", "2", "3", "4", "5", "avg"]) == 3
+    // 15 / 5 = 3
+calculate(["2", "2", "4", "4", "avg"]) == 3
+    // 12 / 4 = 3
+calculate(["2", "avg"]) == 2
+    // 2 / 1 = 2
+calculate(["avg"]) == 0
+    // 0 / 0 = 0 (not really, but it's an edge case
+
+calculate(["1", "fact"]) == 1
+calculate(["1", "fact"]) == 1
+calculate(["2", "fact"]) == 2 // 2*1
+calculate(["5", "fact"]) == 120 // 5*4*3*2*1
+calculate(["fact"]) == 0
+
+calculate("2 + 2") == 4
+calculate("2 * 2") == 4
+calculate("2 - 2") == 0
+calculate("2 / 2") == 1
+
+calculate("1 2 3 4 5 count") == 5
+calculate("1 2 3 4 5 avg") == 3
+calculate("5 fact") == 120
